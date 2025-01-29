51b70fb8d3aa9f22870f9bbd1340c8de23f39992
Nelya <nelya.tairova@gmail.com>
2025-01-29 11:19:37 +0300

    пробел лишний

--- fixture/signup.py
+++ fixture/signup.py
@@ -14,7 +14,6 @@ class SignupHelper:
         wd.find_element_by_xpath("//input[@value='Signup']").click()
         mail = self.app.mail.get_mail(username, password, '[MantisBT] Account registration')
         url = self.extract_confirmation_url(mail)
-
         wd.get(url)
         wd.find_element_by_name("password").send_keys(password)
         wd.find_element_by_name("password_confirm").send_keys(password)
